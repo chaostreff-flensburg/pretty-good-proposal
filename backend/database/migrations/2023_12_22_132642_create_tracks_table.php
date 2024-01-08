7e7de7b951a722e819945f1271ee7669e514ba5d
scammo <sbrinkmann@54gradsoftware.de>
2024-01-08 17:14:13 +0100

    fix migration public_key

--- backend/database/migrations/2023_12_22_132642_create_tracks_table.php
+++ backend/database/migrations/2023_12_22_132642_create_tracks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('public_key');
+            $table->text('public_key');
             $table->timestamps();
         });
     }
