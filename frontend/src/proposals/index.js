9ecff259b6d6bbb32649deb72f6a850db66896d7
dasUnicorn <hi@lana.coffee>
2024-11-22 18:54:35 +0100

    fix: add missing index for new ccs

--- frontend/src/proposals/index.js
+++ frontend/src/proposals/index.js
@@ -1,7 +1,9 @@
 import ccc42 from './ccc-42.json'
 import ccs20241 from './ccs-20241.json'
+import ccs20242 from './ccs-20242.json'
 
 export const proposals = {
     'ccc-42': ccc42,
-    'ccs-20241': ccs20241
+    'ccs-20241': ccs20241,
+    'ccs-20242': ccs20242
 }
