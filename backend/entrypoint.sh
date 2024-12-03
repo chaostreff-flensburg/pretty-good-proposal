be5182c6c726d5376744f5a6c1a209d57487bda2
Flemming Petersen <mail@flemming-petersen.de>
2024-12-03 19:29:51 +0100

    Simplify Backend

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /var/www/html && mkdir -p storage/framework/{sessions,views,cache}
+cd /var/www/html && php artisan storage:link
+cd /var/www/html && php artisan optimize:clear
+cd /var/www/html && php artisan config:cache
+cd /var/www/html && php artisan route:cache
+cd /var/www/html && php artisan view:cache
+cd /var/www/html && php artisan event:cache
+
+# Wait until db is online
+sleep 1
+cd /var/www/html && php artisan migrate --force
+
+exec "$@"
