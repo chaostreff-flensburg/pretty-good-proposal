d7d86887c5af99d1016a4232efb607e592b9bb6a
scammo <sbrinkmann@54gradsoftware.de>
2025-06-02 18:02:51 +0200

    link proposal json

--- frontend/src/proposals/index.js
+++ frontend/src/proposals/index.js
@@ -1,9 +1,11 @@
 import ccc42 from './ccc-42.json'
 import ccs20241 from './ccs-20241.json'
 import ccs20242 from './ccs-20242.json'
+import ccs20251 from './ccs-20251.json'
 
 export const proposals = {
     'ccc-42': ccc42,
     'ccs-20241': ccs20241,
-    'ccs-20242': ccs20242
+    'ccs-20242': ccs20242,
+    'ccs-20251': ccs20251,
 }
