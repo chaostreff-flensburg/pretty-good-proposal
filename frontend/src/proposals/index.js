e97cb3bf54c37d3b32888ff1627003ab49d97ae4
dasUnicorn <hi@lana.coffee>
2025-11-20 20:57:16 +0100

    add new ccs track for 2025 2

--- frontend/src/proposals/index.js
+++ frontend/src/proposals/index.js
@@ -2,10 +2,12 @@ import ccc42 from './ccc-42.json'
 import ccs20241 from './ccs-20241.json'
 import ccs20242 from './ccs-20242.json'
 import ccs20251 from './ccs-20251.json'
+import ccs20252 from './ccs-20252.json'
 
 export const proposals = {
     'ccc-42': ccc42,
     'ccs-20241': ccs20241,
     'ccs-20242': ccs20242,
     'ccs-20251': ccs20251,
+    'ccs-20252': ccs20252,
 }
